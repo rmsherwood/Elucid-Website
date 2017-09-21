b22bb87b3b37374796ba73583a5865bced2d6711
Larry D. Lee jr <llee@jhsph.edu>
2017-09-21 11:35:37 -0400

    [Elucid: Website Project: Create the Banner Block Module Task] Removed the Google WebFont Loader API module as this wasnt working under firefox. Now the Elucid Theme loads the font directly. Also added the banner ribbon to the homepage.

--- themes/elucid/js/elucid.js
+++ themes/elucid/js/elucid.js
@@ -7,6 +7,23 @@
   });
 
   function init () {
+    loadFonts ();
+
+    // Initialize the material design component elements.
     mdc.autoInit ();
+  }
+
+  /*
+    Loads the fonts used by this theme.
+
+    Note: this function relies on the theme library including
+    https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js
+  */
+  function loadFonts () {
+    WebFont.load({
+      google: {
+        families: ['Open+Sans:300,400,600']
+      }
+    });
   }
 }) (jQuery);
